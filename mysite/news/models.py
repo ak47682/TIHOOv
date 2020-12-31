0b9be21b6434d33d9107b18c5baa4f09411e3503
TIHOO <a68226888@163.com>
2020-12-31 16:49:33 +0800

    完成管理以及用户操作视图

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.full_name
+
+class Article(models.Model):
+    pub_date = models.DateField()
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
+        #教程的第一步，定义好model文件
+        #目的是为了通过简单的命令完成数据库的创建
+        '''
+        在此，我们在Article中定义了四个属性
+        其中第一个是日期，第二个则规定了文本，字段最长200
+        第三个则定义了文章的主体，最后加了一个外码，指向reporter，
+        对应reporter中的记者表'''
+
---
+++ mysite/news/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('articles/<int:year>/', views.year_archive),
+    #path('articles/<int:year>/<int:month>/', views.month_archive),
+    #path('articles/<int:year>/<int:month>/<int:pk>/', views.article_detail),
+]
+#此处会出现报错，原因是因为我们并没有写相应的功能，所以我们要继续添加功能
